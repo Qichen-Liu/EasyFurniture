7f4c5506bf2187a6b2498a7441ae5cd571a43a8d
Holly1212 <zh2667@nyu.edu>
2023-10-04 22:33:45 -0400

    test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Basic Math Operations', function () {
+    it('should return 4 when adding 2 and 2', function () {
+        const result = 2 + 2;
+        expect(result).to.equal(4);
+    });
+});
