ef2f267448784290324f52ddc6326b965425f497
Holly1212 <zh2667@nyu.edu>
2023-10-05 21:40:35 -0400

    fix eslint

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-const chai = require("chai");
+const chai = require('chai')
 
-const expect = chai.expect;
+const expect = chai.expect
 
-describe("Basic Math Operations", function () {
-  it("should return 4 when adding 2 and 2", function () {
-    const result = 2 + 2;
-    expect(result).to.equal(4);
-  });
-});
+describe('Basic Math Operations', function () {
+  it('should return 4 when adding 2 and 2', function () {
+    const result = 2 + 2
+    expect(result).to.equal(4)
+  })
+})
