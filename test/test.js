4bf2a3032e91ad9eed72198bf014b2e25df52914
Qichen-Liu <ql2326@nyu.edu>
2023-10-05 21:15:55 -0400

    updated prettieric

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-const chai = require('chai')
-const expect = chai.expect
+const chai = require("chai");
 
-describe('Basic Math Operations', function () {
-  it('should return 4 when adding 2 and 2', function () {
-    const result = 2 + 2
-    expect(result).to.equal(4)
-  })
-})
+const expect = chai.expect;
+
+describe("Basic Math Operations", function () {
+  it("should return 4 when adding 2 and 2", function () {
+    const result = 2 + 2;
+    expect(result).to.equal(4);
+  });
+});
