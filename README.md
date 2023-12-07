ef281589d0a2d351db1375613c8ea473424006d3
EdenWuyifan <yfw215@nyu.edu>
2023-12-06 20:11:56 -0500

    fix format

--- README.md
+++ README.md
@@ -1,50 +1,66 @@
 # team3
-[![Continuous Integration](https://github.com/Qichen-Liu/team3/actions/workflows/continues_integration.yml/badge.svg)](https://github.com/Qichen-Liu/team3/actions/workflows/continues_integration.yml) 
-  
+
+[![Continuous Integration](https://github.com/Qichen-Liu/team3/actions/workflows/continues_integration.yml/badge.svg)](https://github.com/Qichen-Liu/team3/actions/workflows/continues_integration.yml)
+
 This is the repository for team 3
+
 ### 1. Programming Language
+
 - **Selected**: JavaScript/Node.js
 
 ### 2. Toolchain / Runtime Environment
+
 - **Selected**: Node.js
 
 ### 3. Testing Framework
+
 - **Selected**: Mocha
 
 ### 4. Continuous Integration Solution
+
 - **Selected**: GitHub Actions
 
 ### 5. Static Analysis Solution
+
 - **Selected**: ESLint
 
 ### 6. Code Formatting Solution
+
 - **Selected**: Prettier
 
 ### 7. Package Manager Solution
+
 - **Selected**: npm
 
 ### 8. Pre-configured Static Analysis & Code Formatting
+
 - **ESLint** and **Prettier** should be configured as strictly as possible.
 
 ### 9. “Hello World” Program
+
 - `src/helloWorld.js` file:
+
 ```javascript
-console.log('Hello, World!');
+console.log("Hello, World!");
 ```
 
 ### 10. Basic Test
+
 - `test/test.js`:
+
 ```javascript
 const sum = (a, b) => a + b;
-test('adds 2 + 2 to equal 4', () => {
+test("adds 2 + 2 to equal 4", () => {
   expect(sum(2, 2)).toBe(4);
 });
 ```
 
 ### 11. License
+
 - MIT license
 
 ### 12. Usage
+
 - install required modules:
   ```sh
   npm install
--- server.js
+++ server.js
@@ -1,72 +1,74 @@
-require('dotenv').config();
-require('./configs/MongoDBConfig');
-const cors = require('cors');
-const express = require('express')
-const passport = require('./service/passportService')
-const session = require('express-session')
-const { CLIENT_URL, NODE_ENV, SESSION_SECRET, COOKIE_DOMIAN, MONGODB } = require('./configs/config');
-const MongoDBStore = require('connect-mongodb-session')(session);
+require("dotenv").config();
+require("./configs/MongoDBConfig");
+const cors = require("cors");
+const express = require("express");
+const passport = require("./service/passportService");
+const session = require("express-session");
+const {
+  CLIENT_URL,
+  NODE_ENV,
+  SESSION_SECRET,
+  COOKIE_DOMIAN,
+  MONGODB,
+} = require("./configs/config");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 // ------- require routes ------
-const FurnitureRoute = require('./routes/furnitureRoute');
-const LoginRoute = require('./routes/loginRoute');
-
+// const FurnitureRoute = require("./routes/furnitureRoute");
+const LoginRoute = require("./routes/loginRoute");
 
 const app = express();
 
 // ------ CORS ------
-app.enable('trust proxy');
+app.enable("trust proxy");
 app.use(
-    cors({
-        origin:
-            NODE_ENV === 'production'
-                ? CLIENT_URL
-                : function (origin, callback) {
-                    return callback(null, true);
-                },
-        methods: 'GET, PUT, POST, DELETE',
-        credentials: true, // Allow credentials (cookies)
-    }),
+  cors({
+    origin:
+      NODE_ENV === "production"
+        ? CLIENT_URL
+        : function (origin, callback) {
+            return callback(null, true);
+          },
+    methods: "GET, PUT, POST, DELETE",
+    credentials: true, // Allow credentials (cookies)
+  }),
 );
 
 app.use(express.json());
 
-
 // ------ SESSION & COOKIES & PASSPORT ------
 let cookieConfig = {
-    maxAge: 1000 * 60 * 60 * 24,
-    domain: COOKIE_DOMIAN,
+  maxAge: 1000 * 60 * 60 * 24,
+  domain: COOKIE_DOMIAN,
 };
-if (NODE_ENV === 'production' || NODE_ENV === 'development') {
-    cookieConfig.secure = true;
-    cookieConfig.sameSite = 'none';
+if (NODE_ENV === "production" || NODE_ENV === "development") {
+  cookieConfig.secure = true;
+  cookieConfig.sameSite = "none";
 }
 const store = new MongoDBStore({
-    uri: MONGODB,
-    collection: 'sessions', // Collection to store sessions
+  uri: MONGODB,
+  collection: "sessions", // Collection to store sessions
 });
-store.on('error', (error) => {
-    console.error('MongoDBStore Error:', error);
+store.on("error", (error) => {
+  console.error("MongoDBStore Error:", error);
 });
 app.use(
-    session({
-        secret: SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        cookie: cookieConfig,
-        store: store,  // TODO: set and store it in redis
-    }),
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: cookieConfig,
+    store: store, // TODO: set and store it in redis
+  }),
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // ------- USE ROUTES ------
-app.use('/api/login', LoginRoute);
+app.use("/api/login", LoginRoute);
 //app.use('/api/sublease', SubleaseRoute);
-
 
 // ------START SERVER ------
 const port = process.env.PORT;
-console.log('Client Website is now:', CLIENT_URL);
+console.log("Client Website is now:", CLIENT_URL);
 app.listen(port, () => console.log(`Listening on port ${port}!`));
