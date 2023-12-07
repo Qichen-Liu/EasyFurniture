61e02fecc7b90ea2e5d6572186ccc91917a9aefa
qndgsa <qndgsa@gmail.com>
2023-12-06 20:12:07 -0500

    Models ready

--- models/User.js
+++ models/User.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-    {
-        avatar: {
-            type: String,
-            required: true,
-            default: '',
-        },
-        displayName: {
-            type: String,
-            require: true,
-            default: '',
-        },
-        email: {
-            type: String,
-            require: true,
-            default: '',
-        }
+  {
+    avatar: {
+      type: String,
+      required: true,
+      default: "",
     },
-    { collection: 'User' }
-)
+    displayName: {
+      type: String,
+      require: true,
+      default: "",
+    },
+    email: {
+      type: String,
+      require: true,
+      default: "",
+    },
+  },
+  { collection: "User" },
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
