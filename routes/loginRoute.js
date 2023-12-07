07bb10da9ee568c56e4d1383e1aad518cc09af63
Qichen-Liu <ql2326@nyu.edu>
2023-12-06 20:39:30 -0500

    added login route and passport service

---
+++ routes/loginRoute.js
@@ -0,0 +1,60 @@
+const passport = require("passport");
+const express = require("express");
+const router = express.Router();
+const { CLIENT_URL, COOKIE_DOMIAN } = require("../configs/config");
+const {
+  ensureAuthenticated,
+  forwardAuthenticated,
+} = require("../middlewares/loginMiddleware");
+
+// --- Google Login ---
+router.get(
+  "/auth/google",
+  forwardAuthenticated,
+  passport.authenticate("google", { scope: ["profile", "email"] }),
+);
+
+// --- Google Callback ---
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `${CLIENT_URL}`,
+    failureRedirect: `${CLIENT_URL}/signin-google-callback/fail`,
+  }),
+);
+
+// --- Get User Info ---
+router.post("/info", ensureAuthenticated, async (req, res) => {
+  console.log(req.isAuthenticated());
+  console.log(req.user);
+  try {
+    res.status(201).json({
+      code: 0,
+      msg: "success",
+      user: req.user,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "its Server Error" });
+  }
+});
+
+// --- logout ---
+router.post("/logout", ensureAuthenticated, async (req, res) => {
+  req.session.destroy((error) => {
+    if (error)
+      return res.json({
+        code: 2,
+        msg: "fail to logout",
+      });
+
+    res.clearCookie("connect.sid", { domain: COOKIE_DOMIAN });
+    res.clearCookie("G_AUTHUSER_H", { domain: COOKIE_DOMIAN });
+    res.clearCookie("G_ENABLED_IDPS", { domain: COOKIE_DOMIAN });
+    res.status(201).json({
+      code: 0,
+      msg: "success logout",
+    });
+  });
+});
+
+module.exports = router;
---
+++ service/passportService.js
@@ -0,0 +1,54 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const {
+  GOOGLE_CALLBACK_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+} = require("../configs/config");
+const User = require("../models/User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      callbackURL: GOOGLE_CALLBACK_URL,
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        // if user exists, call our callback
+        if (user) {
+          done(null, user);
+        } else {
+          // if new user, create a user object and store it
+          const newUser = new User({
+            avatar: profile.photos[0].value,
+            email: profile.emails[0].value,
+            displayName: profile.displayName,
+          });
+
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  ),
+);
+
+// store user in session by user id
+passport.serializeUser((user, done) => {
+  done(null, user.email);
+});
+
+// function provided earlier to fetch the user object by ID.
+passport.deserializeUser(async (email, done) => {
+  const user = await User.findOne({ email: email });
+  // console.log('deseri user', user)
+  done(null, user);
+});
+
+module.exports = passport;
