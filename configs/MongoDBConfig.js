5cb98cd28e3be48d597173e2e53aca7a4265b1bf
Holly1212 <zh2667@nyu.edu>
2023-12-06 20:12:02 -0500

    format fix

--- configs/MongoDBConfig.js
+++ configs/MongoDBConfig.js
@@ -1,30 +1,32 @@
-const mongoose = require('mongoose');
-const { DATABASE, MONGODB } = require('./config');
+const mongoose = require("mongoose");
+const { DATABASE, MONGODB } = require("./config");
 
 const db = MONGODB;
 
 mongoose.connect(db, {
-    //useUnifiedTopology: true,
-    //useNewUrlParser: true,
-    dbName: DATABASE,
-    maxPoolSize: 100,
+  //useUnifiedTopology: true,
+  //useNewUrlParser: true,
+  dbName: DATABASE,
+  maxPoolSize: 100,
 });
 
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection open to ' + MONGODB);
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose default connection open to " + MONGODB);
 });
 
-mongoose.connection.on('error', function (err) {
-    console.log('Mongoose default connection error: ' + err);
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose default connection error: " + err);
 });
 
-mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose default connection disconnected");
 });
 
-process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
-        throw new Error('Exit');
-    });
+process.on("SIGINT", function () {
+  mongoose.connection.close(function () {
+    console.log(
+      "Mongoose default connection disconnected through app termination",
+    );
+    throw new Error("Exit");
+  });
 });
--- configs/config.js
+++ configs/config.js
@@ -2,48 +2,49 @@
 // const NODE_ENV = 'production';
 
 // Local-Development
-const NODE_ENV = 'local-development';
+const NODE_ENV = "local-development";
 
 // System Server URL
-const PROD_SERVER_URL = ''
-const LOCAL_DEV_SERVER_URL = 'http://localhost:4242';
+const PROD_SERVER_URL = "";
+const LOCAL_DEV_SERVER_URL = "http://localhost:4242";
 
 // System Client URL
-const PROD_CLIENT_URL = ''
-const LOCAL_DEV_CLIENT_URL = 'http://localhost:3000';
+const PROD_CLIENT_URL = "";
+const LOCAL_DEV_CLIENT_URL = "http://localhost:3000";
 
 // MongoDB
-const MONGODB = 'your mongodb config'
-const DATABASE = 'loginSystem'
+const MONGODB = "your mongodb config";
+const DATABASE = "loginSystem";
 
 // Google Authentication
-const GOOGLE_CALLBACK_URL = 'http://localhost:4242/api/login/auth/google/callback';
-const GOOGLE_CLIENT_ID = 'your google client id';
-const GOOGLE_CLIENT_SECRET = 'your google secret';
+const GOOGLE_CALLBACK_URL =
+  "http://localhost:4242/api/login/auth/google/callback";
+const GOOGLE_CLIENT_ID = "your google client id";
+const GOOGLE_CLIENT_SECRET = "your google secret";
 
 // Cookie Domain
-const PROD_COOKIE_DOMAIN = '';
-const LOCAL_COOKIE_DOMAIN = 'localhost';
+const PROD_COOKIE_DOMAIN = "";
+const LOCAL_COOKIE_DOMAIN = "localhost";
 
 // Session Secret
-const SESSION_SECRET = 'shuanqbaby';
+const SESSION_SECRET = "shuanqbaby";
 
 const SERVER_URL =
-    NODE_ENV === 'production' ? PROD_SERVER_URL : LOCAL_DEV_SERVER_URL;
+  NODE_ENV === "production" ? PROD_SERVER_URL : LOCAL_DEV_SERVER_URL;
 const CLIENT_URL =
-    NODE_ENV === 'production' ? PROD_CLIENT_URL : LOCAL_DEV_CLIENT_URL;
+  NODE_ENV === "production" ? PROD_CLIENT_URL : LOCAL_DEV_CLIENT_URL;
 const COOKIE_DOMIAN =
-    NODE_ENV === 'production' ? PROD_COOKIE_DOMAIN : LOCAL_COOKIE_DOMAIN;
+  NODE_ENV === "production" ? PROD_COOKIE_DOMAIN : LOCAL_COOKIE_DOMAIN;
 
 module.exports = {
-    NODE_ENV,
-    SERVER_URL,
-    CLIENT_URL,
-    MONGODB,
-    DATABASE,
-    GOOGLE_CALLBACK_URL,
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    SESSION_SECRET,
-    COOKIE_DOMIAN,
-}
+  NODE_ENV,
+  SERVER_URL,
+  CLIENT_URL,
+  MONGODB,
+  DATABASE,
+  GOOGLE_CALLBACK_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  SESSION_SECRET,
+  COOKIE_DOMIAN,
+};
